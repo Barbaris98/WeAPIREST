637d30b8a40ea2149178646d5479d6162a7801b7
Dmitry Grigorev <d-d12@mail.ru>
2023-08-13 18:14:10 +0400

    MapGet and MapDelete

--- Program.cs
+++ Program.cs
@@ -6,11 +6,42 @@ List<Person> users = new List<Person>
     new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
 };
 
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/api/users", ()=> users);
+app.MapGet("/api/users/{id}", (string id) =>
+{
+    // получаем пользовател€ по id
+    Person? user = users.FirstOrDefault(x => x.Id == id);
+    // если не найден то отпр статусный код об ошибке
+    if (user == null)
+    {
+        return Results.NotFound(new { message = "ѕользователь не найден" });
+    }
+    // если найден, то отправл€ем его
+    return Results.Json(user);
+});
+
+app.MapDelete("/api/users/{id}", (string id) =>
+{
+    // получаем пользовател€ по id
+    Person? user = users.FirstOrDefault(x => x.Id == id);
+    if (user == null)
+    {
+        return Results.NotFound(new { message = "ѕользватель не найден"});
+    }
+    // если найден, то удал€ем его
+    users.Remove(user);
+    return Results.Json(user);
+});
+
+
+
+
 
 app.Run();
 
